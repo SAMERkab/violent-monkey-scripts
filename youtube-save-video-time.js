b3b0c97ae856f0467a5285b25dff7e2b7c3c243c
SAMERkab <samerkab2001@gmail.com>
2025-07-29 19:54:15 +0200

    Switch to periodically persisting video time in the script store to improve reliability

--- youtube-save-video-time.js
+++ youtube-save-video-time.js
@@ -1,19 +1,14 @@
 // ==UserScript==
-// @name        Update the url to include the current playback time
+// @name        Persist video playback time
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/*
 // @grant GM.getValue
 // @grant GM.setValue
 // @grant GM.deleteValue
-// @version     1.0
+// @version     2.0
 // @author      -
 // @description 19/07/2025, 16:12:36
 // ==/UserScript==
-
-/*
-  window.history.replaceState({}, "", urlWithTime);
-  window.location.replace(urlWithTime);
-*/
 
 class Video {
   id;
@@ -23,13 +18,17 @@ class Video {
 
   static async fromThisPage() {
     const video = new Video();
+    await video.findElementAndId();
+    return video;
+  }
 
-    return new Promise(function(resolve, reject) {
+  async findElementAndId() {
+    return new Promise((resolve, reject) => {
       function searchForVideo() {
-        video.updateElement();
-        if (video.element) {
-          video.updateId();
-          resolve(video);
+        this.setElementFromDocument();
+        if (this.element) {
+          this.setIdFromUrl();
+          resolve(true);
         } else {
           setTimeout(searchForVideo, 250);
         }
@@ -38,21 +37,18 @@ class Video {
     });
   }
 
-  constructor() {
-    startSavingCurrentTime();
-  }
-
-  updateElement() {
+  setElementFromDocument() {
     this.element = document.querySelector("#primary video");
   }
 
-  updateId() {
-    this.id = (new URL(window.location.href)).searchParams.get("v");;
+  setIdFromUrl() {
+    this.id = new URL(window.location.href).searchParams.get("v");
   }
 
   startSavingCurrentTime() {
     this.currentTimeInterval = setInterval(() => {
       this.currentTime = this.element?.currentTime;
+      this.storeCurrentTime();
     }, 5000);
   }
 
@@ -61,8 +57,10 @@ class Video {
   }
 
   async storeCurrentTime() {
+    if (!Number.isFinite(this.currentTime)) return;
+
     if (this.currentTime >= 5) {
-      await GM.setValue(this.id, Math.floor(this.currentTime ?? 0));
+      await GM.setValue(this.id, Math.floor(this.currentTime));
     }
     if (this.currentTime >= this.element.duration - 5) {
       await GM.deleteValue(this.id);
@@ -72,7 +70,11 @@ class Video {
   async seekStoredTime() {
     const savedTime = await GM.getValue(this.id);
     if (savedTime) {
-      this.element.fastSeek(savedTime);
+      if (this.element.fastSeek) {
+        this.element.fastSeek(savedTime);
+      } else {
+        this.element.currentTime = savedTime;
+      }
     }
   }
 }
@@ -81,22 +83,14 @@ function pageHasVideo() {
   return window.location.href.includes("watch");
 }
 
-let video = null;
+let video = new Video();
+console.log("script loaded: Persist video playback time");
 
 window.addEventListener("load", async (event) => {
   if (pageHasVideo()) {
-    video = await Video.fromThisPage();
+    await video.findElementAndId();
     await video.seekStoredTime();
-  }
-});
-
-window.addEventListener("popstate", async (event) => {
-  if (pageHasVideo()) {
-    video = await Video.fromThisPage();
-    await video.seekStoredTime();
-  } else {
-    await video?.storeCurrentTime();
-    video?.stopSavingCurrentTime();
+    video.startSavingCurrentTime();
   }
 });
 
@@ -105,24 +99,12 @@ window.addEventListener("beforeunload", async (event) => {
   video?.stopSavingCurrentTime();
 });
 
-
-
-/*
-function createUrlWithPlaybackTime(playbackTime) {
-  const parsedUrl = new URL(window.location.href);
-  parsedUrl.searchParams.set("t", playbackTime);
-  return parsedUrl.href;
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener("popstate", async (event) => {
+  if (pageHasVideo()) {
+    await video.findElementAndId();
+    await video.seekStoredTime();
+    video.startSavingCurrentTime();
+  } else {
+    video?.stopSavingCurrentTime();
+  }
+});
